30e6280e55ad658bbc01a3488ca17dac798ac866
cesar0987 <37241515+cesar0987@users.noreply.github.com>
2024-04-25 18:50:54 -0400

    Changes on js

--- js/index.js
+++ js/index.js
@@ -1,9 +1,79 @@
 const showText = document.querySelector("#display")
-let number1Saves=0;
-let number2Saves=0;
-let result=0;
+let numberPressed = '0';
+let operator = '';
+let firstOperand = '';
+let secondOperand = '';
 
-function press(number){
-    numberPressed= number
-    showText.textContent=numberPressed
+
+function press(number) {
+    if (showText.textContent === 'ERROR') {
+        showText.textContent = '0';
+        numberPressed = '';
+    }
+
+    
+    if (numberPressed.length < 10) { 
+        
+        if (number === '.') {
+            if (!numberPressed.includes('.')) {
+                numberPressed += number;
+            }
+        } else {
+            if (numberPressed === '0') {
+                numberPressed = number.toString();
+            } else {
+                numberPressed += number.toString();
+            }
+        }
+
+        showText.textContent = numberPressed;
+    }
+}
+function setOP(op) {
+    operator = op;
+    firstOperand = numberPressed;
+    showText.textContent = '0';
+    numberPressed = '0';
+}
+
+function calculate() {
+    secondOperand = numberPressed;
+
+    const num1 = parseFloat(firstOperand);
+    const num2 = parseFloat(secondOperand);
+
+    let result;
+
+    switch (operator) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            if (num2 !== 0) {
+                result = num1 / num2;
+            } else {
+                result = 'ERROR'; 
+            }
+            break;
+        default:
+            result = 'ERROR';
+            break;
+    }
+
+    showText.textContent = result.toString().slice(0, 10);
+
+    operator = '';
+    firstOperand = '';
+    secondOperand = '';
+}
+
+function clr() {
+    showText.textContent = '0';
+    numberPressed = '0';
 }
